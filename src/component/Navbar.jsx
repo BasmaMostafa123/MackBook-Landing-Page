a6f450101c2a8e03e770ebf0d9f941d7c66710d9
Basma Mostafa <basmamostafaelbasyoni@gmail.com>
2026-05-12 21:30:32 +0300

    Add full project files

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <img src="/logo.svg" alt="Apple Logo" />
+
+        <ul>
+          {[
+            { label: "Store" },
+            { label: "Mac" },
+            { label: "iPhone" },
+            { label: "Watch" },
+            { label: "Vision" },
+            { label: "Airpods" }
+          ].map(({ label }) => (
+            <li key={label}>
+              <a href={label}>{label}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="flex-center gap-3">
+          <button>
+            <img src="/search.svg" alt="Search" />
+          </button>
+
+          <button>
+            <img src="/cart.svg" alt="Cart" />
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar; 
